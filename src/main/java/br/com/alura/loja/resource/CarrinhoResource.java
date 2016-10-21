00b4d752f0e4fba6e37a324c72c8b74c2d215dfd
Wellington Batista <wellingtonex@gmail.com>
2016-10-21 12:02:03 -0300

    ajustando metodo adicionar

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -2,6 +2,7 @@ package br.com.alura.loja.resource;
 
 import java.net.URI;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -27,7 +28,7 @@ public class CarrinhoResource {
 	}
 	
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
+	@Consumes(MediaType.APPLICATION_XML)
 	public Response adiciona(String conteudo) {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		new CarrinhoDAO().adiciona(carrinho);
