2c0288adbfff6b618c475a36869f833ed2fb984f
Wellington Batista <wellingtonex@gmail.com>
2016-10-21 12:16:56 -0300

    criando teste unitario para testar o metodo adicionar carrinho

--- src/test/java/br/com/alura/loja/ClientTest.java
+++ src/test/java/br/com/alura/loja/ClientTest.java
@@ -2,20 +2,50 @@ package br.com.alura.loja;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import junit.framework.Assert;
 
 public class ClientTest {
+	
+	private WebTarget target;
+	private Client client;
+
+	@Before
+	public void init() {
+		client = ClientBuilder.newClient();
+		target = client.target("http://localhost:8080");
+	}
 
 	@Test
 	public void testaQueAConexaoComOServidorFunciona() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("Rua Vergueiro 3185"));
 		System.out.println(conteudo);
+	}
+	
+	@Test
+	public void testaQueSuportaNovosCarrinhos() {
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314, "Microfone", 37, 1));
+		carrinho.setRua("Rua Vergueiro 3185");
+		carrinho.setCidade("São Paulo");
+		String xml = carrinho.toXML();
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Microfone"));
 	}
 }
